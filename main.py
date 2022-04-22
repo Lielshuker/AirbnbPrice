79487f60b16464fd94c2c89334e28494ff11f218
gilor19 <gilor19@gmail.com>
2022-04-22 19:56:05 +0300

    modified run

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from data_preparation.cleaning import load_and_clean_data
-from data_preparation.wrangling_and_feature_engineering import *
+from data_preparation.load_data import column_type
+from data_preparation.wrangling_and_feature_engineering import feature_engineering
 from model.evaluate_results import evaluate_results
-from model.model_analysis import model_analysis
 from model.train import train_model
 
 
 def run():
+    columns_dict = column_type()
     train_with_mean, test = load_and_clean_data(path='dataset/train.csv')
-    train, test = feature_engineering(train_with_mean, test)
+    train = train_with_mean['data_with_most_frequent']
+    train, test = feature_engineering(train, test, columns_dict)
     model = train_model(train)
     evaluate_results(model=model, test=test, train=train)
+    print('')
 
 
 if __name__ == '__main__':
