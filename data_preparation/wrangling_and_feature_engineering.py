d413099fa83f7ae19d8a405d720a4db3489cbc39
gilor19 <gilor19@gmail.com>
2022-04-22 19:55:26 +0300

    added feature_engineering func

--- data_preparation/wrangling_and_feature_engineering.py
+++ data_preparation/wrangling_and_feature_engineering.py
@@ -1,6 +1,4 @@
 import pandas as pd
-from cleaning import load_and_clean_data
-from datetime import datetime
 import numpy as np
 
 from data_preparation.load_data import convert_present_to_float
@@ -22,9 +20,9 @@ def collect_amenities(data):
 def create_amenities_array(amenities, data):
     amenities_list = list(amenities)
     amenities_array = []
-    for row in range(0, data.shape[0]):
+    for index, row in data.iterrows():
         array = np.zeros(shape=(len(amenities)))
-        row_amen = data['amenities'][row].split(',')
+        row_amen = data['amenities'][index].split(',')
         for amen in row_amen:
             item = amen.replace('"', '').replace('}', '').replace('{', '')
             if "translation missing" not in item and item !='':
@@ -52,11 +50,6 @@ def handle_amenities(data, columns_dict):
 
 def randomly_shuffle_training_data(train):
     return train.sample(frac=1)
-
-
-def convert_date_cols_to_datetime(data, cols_list):
-    for col in cols_list:
-       data[col] = pd.to_datetime(data[col])
 
 
 def handle_neighbourhood(data):
@@ -132,6 +125,17 @@ def equalize_columns(train, test):
 
     return train, test
 
+
+def feature_engineering(train, test, columns_dict):
+    train = handle_neighbourhood(train)
+    # train, columns_dict = handle_amenities(train, columns_dict)
+    # test, columns_dict = handle_amenities(test, columns_dict)
+    naive_binning(train, test, columns_dict)
+    oh_train, oh_test = one_hot_enc(train, test, columns_dict)
+    concat_binary_cols(train, test, oh_train, oh_test, columns_dict)
+    oh_train, oh_test = equalize_columns(oh_train, oh_test)
+
+    return oh_train, oh_test
 
 if __name__ == '__main__':
     path='/home/gilor/Documents/msc/ds_workshop/AirbnbPrice/dataset/train.csv'
